cc0e2f06a6b39b23c52297e63854cfa926014ced
Christopher Lovett <cjsonic02@hotmail.com>
2022-06-16 19:12:24 -0400

    Added a fibonacci solution

---
+++ LeetCode_Solutions/fib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_Solutions
+{
+    /// <summary>
+    /// Class for solving fibonacci sequence
+    /// </summary>
+    public class fib
+    {
+        public static int solution(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                int a = solution(n - 1);
+                int b = solution(n - 2);
+                return a + b;
+            }
+        }
+    }
+}
---
+++ LeetCode_Solutions_Tests/Test_fib.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_Solutions;
+
+
+namespace LeetCode_Solutions_Tests
+{
+    [TestClass]
+    public class Test_fib
+    {
+        [TestMethod]
+        public void Test_Solution()
+        {
+            Assert.AreEqual(1, fib.solution(1));
+            Assert.AreEqual(1, fib.solution(2));
+            Assert.AreEqual(2, fib.solution(3));
+            Assert.AreEqual(3, fib.solution(4));
+            Assert.AreEqual(5, fib.solution(5));
+            Assert.AreEqual(8, fib.solution(6));
+            Assert.AreEqual(13, fib.solution(7));
+            Assert.AreEqual(21, fib.solution(8));
+            System.Console.WriteLine(fib.solution(15));
+        }
+    }
+}
