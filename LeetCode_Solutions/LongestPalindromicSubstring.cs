8c4bddb082282a089fdac4ca97d7a02ae3c22485
Chris Lovett <chris.lovett02@yahoo.net>
2023-03-02 22:15:45 -0500

    Started the Palindromic Substring solution class and test class.

---
+++ LeetCode_Solutions/LongestPalindromicSubstring.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace LeetCode_Solutions
+{
+    public class LongestPalindromicSubstring
+    {
+        public static string Solution(string word)
+        {
+            string answer = "";
+
+            return answer;
+        }
+    }
+}
---
+++ LeetCode_Solutions_Tests/Test_LongestPalindromicSubstring.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_Solutions;
+
+
+namespace LeetCode_Solutions_Tests
+{
+    [TestClass]
+    public class Test_LongestPalindromicSubstring
+    {
+        [TestMethod]
+        public void Test_LongestPalindromicSubstring_Solution()
+        {
+
+        }
+    }
+}
