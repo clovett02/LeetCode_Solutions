ce814902e807ebf4379e2c649b4fee8c0f36dda8
Chris Lovett <chris.lovett02@yahoo.net>
2023-03-03 15:18:10 -0500

    Added a test for the ZigZagConversion Solution

---
+++ LeetCode_Solutions_Tests/Test_ZigZagConversion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_Solutions;
+
+namespace LeetCode_Solutions_Tests
+{
+    [TestClass]
+    public class Test_ZigZagConversion
+    {
+        [TestMethod]
+        public void Test_ZigZagConversion_Solution()
+        {
+            ZigZagConversion zigzag = new ZigZagConversion();
+
+            Assert.AreEqual("pahnplsiigyir", zigzag.Solution("paypalishiring", 3));
+        }
+    }
+}
