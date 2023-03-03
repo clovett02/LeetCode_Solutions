86826a749204bd512c1c7a3f8014a6bcd686eca1
Chris Lovett <chris.lovett02@yahoo.net>
2023-03-02 23:28:46 -0500

    Added a function to the solution to return substrings given a word and indexs and updated tests.

--- LeetCode_Solutions/LongestPalindromicSubstring.cs
+++ LeetCode_Solutions/LongestPalindromicSubstring.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Globalization;
 
 namespace LeetCode_Solutions
 {
@@ -7,8 +7,61 @@ namespace LeetCode_Solutions
     {
         public static string Solution(string word)
         {
-            string answer = "";
+            ///<summary>
+            ///Takes a string and returns the longest Palindromic substring
+            ///within it.
+            ///(The longest substring that is the same spelled forward or backward)
+            ///</summary>
+            string answer = word[0].ToString();
+            int i = 0;
+            int j = word.Length - 1;
+            int currentsubwordlength = word.Length - 1;
 
+            while (currentsubwordlength > 1)
+            {
+
+                currentsubwordlength--;
+            }
+
+            return answer;
+        }
+        public static string ReturnSubstring(string word, int index1, int index2)
+        {
+            ///<summary>
+            ///Returns a string of characters in the given word from index1 to index2
+            /// </summary>
+
+            string result = "";
+
+            for (int i = index1; i <= index2; i++)
+            {
+                result += word[i];
+            }
+
+            return result;
+
+        }
+        public static bool Palindromic(string word)
+        {
+            ///<summary>
+            ///Returns true if the given string of characters is the same spelled
+            ///forward and backward.
+            ///</summary>
+            bool answer = true;
+            int j = word.Length - 1;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == word[j]) 
+                {
+                    j--;
+                    continue; 
+                }
+                else 
+                { 
+                    answer = false;
+                    break;
+                }
+            }
             return answer;
         }
     }
--- LeetCode_Solutions_Tests/Test_LongestPalindromicSubstring.cs
+++ LeetCode_Solutions_Tests/Test_LongestPalindromicSubstring.cs
@@ -10,7 +10,21 @@ namespace LeetCode_Solutions_Tests
         [TestMethod]
         public void Test_LongestPalindromicSubstring_Solution()
         {
-
+            Assert.AreEqual("bob", LongestPalindromicSubstring.Solution("kabob"));
+            Assert.AreEqual("babad", LongestPalindromicSubstring.Solution("bab"));
+        }
+        [TestMethod]
+        public void Test_LongestPalindromicSubstring_ReturnSubstring()
+        {
+            Assert.AreEqual("abcde", LongestPalindromicSubstring.ReturnSubstring("abcde", 0, 4));
+            Assert.AreEqual("abcde", LongestPalindromicSubstring.ReturnSubstring("abcdef", 0, 4));
+            Assert.AreEqual("bcdef", LongestPalindromicSubstring.ReturnSubstring("abcdefg", 1, 5));
+        }
+        [TestMethod]
+        public void Test_Palindromic()
+        {
+            Assert.AreEqual(true, LongestPalindromicSubstring.Palindromic("aba"));
+            Assert.AreEqual(false, LongestPalindromicSubstring.Palindromic("abb"));
         }
     }
 }
