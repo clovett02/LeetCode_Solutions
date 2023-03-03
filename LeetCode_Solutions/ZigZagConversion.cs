9ee362db571843bfbaf150df4e18800153ef0082
Chris Lovett <chris.lovett02@yahoo.net>
2023-03-03 15:18:46 -0500

    Added documentation for the ZigZagConversion Solution

---
+++ LeetCode_Solutions/ZigZagConversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    public class ZigZagConversion
+    {
+        /// <summary>
+        /// This solution takes a string and returns what the string would be
+        /// if printed in a zigzag pattern. The return string returns how it 
+        /// would be read from left to right, from row 0 to n.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="rows"></param>
+        /// <returns></returns>
+        public string Solution(string word, int rows)
+        {
+            string answer = "";
+
+
+
+            return answer;
+        }
+    }
+}
