977a7368959d01a6e9004c2759cc870887aa47d4
Chris Lovett <chris.lovett02@yahoo.net>
2023-03-03 22:43:46 -0500

    Added a function and tests for returning the number of required columns given word length and the number of rows.

--- LeetCode_Solutions/ZigZagConversion.cs
+++ LeetCode_Solutions/ZigZagConversion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,70 @@ namespace LeetCode_Solutions
         public string Solution(string word, int rows)
         {
             string answer = "";
+            string[,] array_answer = new string[rows, this.ReturnNumberofColumns(word, rows)];
+            int i = 0; //tracks word index
+            int j = 0; //tracks row index
+            int k = 0; //tracks column index
 
-
+            while (i < word.Length)
+            {
+                j = 0;
+                while (j <= rows && i < word.Length)
+                {
+                    array_answer[j, k] = word[i].ToString();
+                    i++;
+                    j++;
+                }
+                j -= 2;
+                k++;
+                while (j > 0)
+                {
+                    array_answer[j, k] = word[i].ToString();
+                    i++;
+                    j--;
+                    k++;
+                }
+            }
 
             return answer;
         }
+
+        public string[,] Return2DArray(string word, int numrows, int numcolumns)
+        {
+            string[,] result = new string[numrows, numcolumns];
+
+            return result;
+        }
+
+        public int ReturnNumberofColumns(string word, int rows)
+        {
+            int result = 0;
+            // if (word.Length > 0) { result = 1; }
+            int i = 0, j = 0;
+            while(i < word.Length)
+            {
+                j = 0;
+                result++;
+                while (j < rows && i < word.Length)
+                {
+                    i++;
+                    j++;
+                }
+                
+                j -= 2;
+                while(j > 0 && i < word.Length)
+                {
+                    i++;
+                    j--;
+                    if(j > 0) { result++; }
+                }
+            }
+
+            return result;
+        }
     }
 }
+
+
+/// 5 chars 2 rows means 3 columns
+/// 5 chars 3 rows means 2 columns
--- LeetCode_Solutions_Tests/Test_ZigZagConversion.cs
+++ LeetCode_Solutions_Tests/Test_ZigZagConversion.cs
@@ -6,12 +6,24 @@ namespace LeetCode_Solutions_Tests
     [TestClass]
     public class Test_ZigZagConversion
     {
+        ZigZagConversion zigzag = new ZigZagConversion();
+        public Test_ZigZagConversion()
+        {
+
+        }
         [TestMethod]
         public void Test_ZigZagConversion_Solution()
         {
-            ZigZagConversion zigzag = new ZigZagConversion();
-
             Assert.AreEqual("pahnplsiigyir", zigzag.Solution("paypalishiring", 3));
+        }
+        [TestMethod]
+        public void Test_ZigZagConversion_ReturnNumberofColumns()
+        {
+            Assert.AreEqual(2, zigzag.ReturnNumberofColumns("abc", 2));
+            Assert.AreEqual(3,zigzag.ReturnNumberofColumns("abcde", 2));
+            Assert.AreEqual(2, zigzag.ReturnNumberofColumns("abcde", 4));
+            Assert.AreEqual(1, zigzag.ReturnNumberofColumns("abcde", 5));
+            Assert.AreEqual(5, zigzag.ReturnNumberofColumns("abcde", 1));
         }
     }
 }
