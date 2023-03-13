80b223e71c56b4ae4b3e18a038b0084dd6431844
Chris Lovett <chris.lovett02@yahoo.net>
2023-03-13 13:00:10 -0400

    minor format change

--- LeetCode_Solutions_Tests/Test_ZigZagConversion.cs
+++ LeetCode_Solutions_Tests/Test_ZigZagConversion.cs
@@ -20,7 +20,7 @@ namespace LeetCode_Solutions_Tests
         public void Test_ZigZagConversion_ReturnNumberofColumns()
         {
             Assert.AreEqual(2, zigzag.ReturnNumberofColumns("abc", 2));
-            Assert.AreEqual(3,zigzag.ReturnNumberofColumns("abcde", 2));
+            Assert.AreEqual(3, zigzag.ReturnNumberofColumns("abcde", 2));
             Assert.AreEqual(2, zigzag.ReturnNumberofColumns("abcde", 4));
             Assert.AreEqual(1, zigzag.ReturnNumberofColumns("abcde", 5));
             Assert.AreEqual(5, zigzag.ReturnNumberofColumns("abcde", 1));
