910dd4852a6362bbf9fb89cd04cbfcee3a1ae59e
Chris Lovett <chris.lovett02@outlook.com>
2023-11-11 05:17:23 -0500

    Completed and submitted LeetCode Solution successfully

--- LeetCode_Solutions/Longest_Common_Prefix.cs
+++ LeetCode_Solutions/Longest_Common_Prefix.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LeetCode_Solutions
 {
@@ -7,17 +8,20 @@ namespace LeetCode_Solutions
         /// <summary>
         /// Problem: Write a function to find the longest common prefix string amongst an
         /// array of strings.
+        /// **Edge Case: if the array is only 1 word long, return that word.
+        /// **Edge Case: if the length of any string in the array is 0, return ""
         /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
         public static string Solution(string[] input)
         {
-            string defaultresponse = "There is no common prefix among the input strings.";
-            if( input.Length <= 1 ){ return defaultresponse; }
-            if( input[0][0] != input[1][0] ){ return defaultresponse; }
-            
             string result = "";
             int shortestword = ShortestWord(input);
+            
+            if( input.Length <= 1 ){ return input[0]; }
+            if( shortestword == 0 ){ return result; }
+            if( input[0][0] != input[1][0] ){ return result; }
+            
 
             //char loop
             for (int i = 0; i < shortestword; i++)
@@ -52,11 +56,6 @@ namespace LeetCode_Solutions
                 if(input[i].Length < result){ result=input[i].Length; }
             }
             return result;
-        }
-        public static bool CharMatch(string word1, string word2, int charnumber)
-        {
-            if(word1[charnumber] == word2[charnumber]){return true;}
-            else{return false;}
         }
     }
 }
--- LeetCode_Solutions_Tests/Test_Longest_Common_Prefix.cs
+++ LeetCode_Solutions_Tests/Test_Longest_Common_Prefix.cs
@@ -6,13 +6,26 @@ namespace LeetCode_Solutions_Tests
     [TestClass]
     public class Test_Longest_Common_Prefix
     {
+        string[] testarray1 = {  "abc", "apple", "axe" };
+        string[] testarray2 = { "black" };
+        string[] testarray3 = { "blue", "purple" };
+        string[] testarray4 = { "opal", "opiate", "opaque" };
+        string[] testarray5 = { "", "" };
+        string[] testarray6 = { "abc", "" };
         [TestMethod]
-        public void Test_Longest_Common_Prefix_Solution()
+        public void Test_Solution()
+        {   
+            Assert.AreEqual( "a", Longest_Common_Prefix.Solution(testarray1));
+            Assert.AreEqual( "black", Longest_Common_Prefix.Solution(testarray2));
+            Assert.AreEqual( "", Longest_Common_Prefix.Solution(testarray3));
+            Assert.AreEqual( "op", Longest_Common_Prefix.Solution(testarray4));
+        }
+        [TestMethod]
+        public void Test_ShortestWord()
         {
-            string[] testarray1 = {
-                "abc", "apple", "axe"
-            };
-            Assert.AreEqual("a", Longest_Common_Prefix.Solution(testarray1));
+            Assert.AreEqual(4, Longest_Common_Prefix.ShortestWord(testarray4));
+            Assert.AreEqual(0, Longest_Common_Prefix.ShortestWord(testarray5));   
+            Assert.AreEqual(0, Longest_Common_Prefix.ShortestWord(testarray6));
         }
     }
 }
